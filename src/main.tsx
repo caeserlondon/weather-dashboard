27a25a2938b39e8f770b24bda3ec251153b77fef
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-01-26 20:33:31 +0700

    Update the main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+const queryClien = new QueryClient();
+
+const container = document.getElementById('root');
+if (container) {
+	createRoot(container).render(
+		<StrictMode>
+			<QueryClientProvider client={queryClien}>
+				<App />
+			</QueryClientProvider>
+		</StrictMode>,
+	);
+}
